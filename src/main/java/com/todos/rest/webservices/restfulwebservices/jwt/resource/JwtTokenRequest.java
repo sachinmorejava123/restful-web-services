41d0cfa89380074c47a1a949285db58f8d6059fe
sachin more <sachin.more@polyglots.io>
2020-09-19 13:26:10 +0530

    first commit

---
+++ src/main/java/com/todos/rest/webservices/restfulwebservices/jwt/resource/JwtTokenRequest.java
@@ -0,0 +1,41 @@
+package com.todos.rest.webservices.restfulwebservices.jwt.resource;
+
+import java.io.Serializable;
+
+public class  JwtTokenRequest implements Serializable {
+  
+  private static final long serialVersionUID = -5616176897013108345L;
+
+  private String username;
+    private String password;
+    
+//    {
+//    	"token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJpbjI4bWludXRlcyIsImV4cCI6MTU5NTEzOTc4OSwiaWF0IjoxNTk0NTM0OTg5fQ._sz5TH-Fu6abcUCklplmm4-TljIMPE8J0grQWK4hsJTXddQC86NUi9II0Var5OnUrCjcyby1W99iQR6zC4nY6Q"
+//    }
+
+    public JwtTokenRequest() {
+        super();
+    }
+
+    public JwtTokenRequest(String username, String password) {
+        this.setUsername(username);
+        this.setPassword(password);
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
---
+++ src/main/resources/data.sql
@@ -0,0 +1,29 @@
+insert into todo(id, username, description, target_date, is_done)
+values(10001, 'sachin', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10002, 'sachin', 'Learn data JPA', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10003, 'sachin', 'Learn Microservices', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10004, 'sachin', 'Learn GraphQL', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10005, 'sachin', 'Learn Angular Material', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10006, 'sachin', 'Learn Json Server', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10007, 'sachin', 'Learn Spring boot', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10008, 'sachin', 'Learn Angular Testing', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10009, 'sachin', 'Learn SQL', sysdate(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values(10010, 'sachin', 'Learn AWS', sysdate(), false);
